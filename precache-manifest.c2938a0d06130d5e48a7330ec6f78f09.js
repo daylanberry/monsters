3ce95cbe839fa28437bec14322dcb2c70353d8aa
Daylan <daylanberry@gmail.com>
2019-12-18 22:31:55 -0800

    Updates

---
+++ precache-manifest.c2938a0d06130d5e48a7330ec6f78f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc80bc75bfe8cfabaf22f255f4d89aee",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b49e3d5ed0d833cce122",
+    "url": "/monsters/static/css/main.8f3968ab.chunk.css"
+  },
+  {
+    "revision": "1b43d2eaa999d2a50f77",
+    "url": "/monsters/static/js/2.c698f0ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.c698f0ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b49e3d5ed0d833cce122",
+    "url": "/monsters/static/js/main.fa355037.chunk.js"
+  },
+  {
+    "revision": "ba67cd212ced8a3c1e1d",
+    "url": "/monsters/static/js/runtime-main.4be90965.js"
+  }
+]);
